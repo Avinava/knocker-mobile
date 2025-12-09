5a886e86edf141955935e9f31f2928debd654f2d
avi <avidev9@gmail.com>
2025-12-09 10:34:45 +0530

    fix expo build

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
-      'react-native-reanimated/plugin',
+    ],
+    plugins: [
       [
         'module-resolver',
         {
@@ -21,6 +22,7 @@ module.exports = function (api) {
           },
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
